4496073833ac00e45ffb23387c506d8f100400c6
Matt Morgis <matt@rappiddevelopment.com>
2017-04-23 17:22:32 -0400

    Initial commit with tests in place

---
+++ src/NumberConverter.java
@@ -0,0 +1,10 @@
+//- Courtesy of James Grenning (@jwgrenning) and Jeff Langr (@jlangr)
+//- Use this to learn TDD
+
+class NumberConverter {
+
+    static String toWords(int number) {
+
+        throw new RuntimeException();
+    }
+}
